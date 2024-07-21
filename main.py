0f6e5f9d4ec34696797de2f8a0e0253916f7d159
Jakob__init__ <jsuetfels@gmail.com>
2024-07-21 15:46:22 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import pygame
 import sys
+import random
 
 pygame.init()
 
 WIDTH, HEIGHT = 800, 600
 win = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption('Platformer')
+pygame.display.set_caption('Platformer with Obstacles')
 
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 BLUE = (0, 0, 255)
+BG_COLOR = (135, 206, 235)
+RED = (255, 0, 0)
 
 clock = pygame.time.Clock()
 
@@ -18,15 +21,24 @@ player_pos = [WIDTH // 2, HEIGHT - 2 * player_size]
 player_vel = [0, 0]
 player_speed = 5
 player_jump = -15
-player_on_ground = False
 player_jump_count = 0
 max_jumps = 2
+scroll_speed = 5
 
-platforms = [
-    [0, HEIGHT - 20, WIDTH, 20],
-    [WIDTH // 3, HEIGHT - 100, 200, 10],
-    [WIDTH * 2 // 3, HEIGHT - 200, 200, 10]
+floor = [0, HEIGHT - 20, WIDTH, 20]
+obstacles = [
+    [WIDTH + 200, HEIGHT - 70, 50, 50],
+    [WIDTH + 600, HEIGHT - 70, 50, 50],
+    [WIDTH + 1000, HEIGHT - 70, 50, 50],
 ]
+
+score = 0
+font = pygame.font.SysFont('Arial', 30)
+
+def generate_obstacle():
+    x = random.randint(WIDTH, WIDTH + 400)
+    y = HEIGHT - 70
+    return [x, y, 50, 50]
 
 running = True
 while running:
@@ -39,7 +51,7 @@ while running:
         player_pos[0] -= player_speed
     if keys[pygame.K_d]:
         player_pos[0] += player_speed
-    if keys[pygame.K_SPACE] and player_jump_count <= max_jumps:
+    if keys[pygame.K_SPACE] and player_jump_count < max_jumps:
         player_vel[1] = player_jump
         player_jump_count += 1
 
@@ -53,15 +65,25 @@ while running:
 
     player_on_ground = False
 
-    for platform in platforms:
-        if (player_pos[1] + player_size >= platform[1] and
-                player_pos[1] + player_size - player_vel[1] <= platform[1] and
-                player_pos[0] + player_size > platform[0] and
-                player_pos[0] < platform[0] + platform[2]):
-            player_pos[1] = platform[1] - player_size
-            player_vel[1] = 0
-            player_on_ground = True
-            player_jump_count = 0
+    if (player_pos[1] + player_size >= floor[1] and
+            player_pos[1] + player_size - player_vel[1] <= floor[1] and
+            player_pos[0] + player_size > floor[0] and
+            player_pos[0] < floor[0] + floor[2]):
+        player_pos[1] = floor[1] - player_size
+        player_vel[1] = 0
+        player_on_ground = True
+        player_jump_count = 0
+
+    for obstacle in obstacles:
+        if (player_pos[1] + player_size >= obstacle[1] and
+                player_pos[1] <= obstacle[1] + obstacle[3] and
+                player_pos[0] + player_size > obstacle[0] and
+                player_pos[0] < obstacle[0] + obstacle[2]):
+            if player_vel[1] > 0:
+                player_vel[1] = 0
+                player_pos[1] = obstacle[1] - player_size
+            else:
+                player_pos[0] = obstacle[0] - player_size
 
     if player_pos[1] > HEIGHT - player_size:
         player_pos[1] = HEIGHT - player_size
@@ -69,12 +91,28 @@ while running:
         player_on_ground = True
         player_jump_count = 0
 
-    win.fill(WHITE)
+    for obstacle in obstacles:
+        obstacle[0] -= scroll_speed
 
-    for platform in platforms:
-        pygame.draw.rect(win, BLACK, platform)
+    for obstacle in obstacles:
+        if obstacle[0] + obstacle[2] < 0:
+            obstacles.remove(obstacle)
+            new_obstacle = generate_obstacle()
+            obstacles.append(new_obstacle)
+
+    score += 1
+
+    win.fill(BG_COLOR)
+
+    pygame.draw.rect(win, BLACK, floor)
+
+    for obstacle in obstacles:
+        pygame.draw.rect(win, RED, obstacle)
 
     pygame.draw.rect(win, BLUE, (*player_pos, player_size, player_size))
+
+    score_text = font.render(f'Score: {score}', True, BLACK)
+    win.blit(score_text, (10, 10))
 
     pygame.display.flip()
 
